fe8e31191715aebf417e6246603fd81ef714f6c1
Jen Show <jennifer.show@gmail.com>
2018-01-07 00:56:50 -0600

    duplicate checker working

--- server/routes/pets.router.js
+++ server/routes/pets.router.js
@@ -3,11 +3,12 @@ var router = express.Router();
 var pool = require('../modules/pool.js');
 
 
-router.get('/', function (req, res) {
+router.get('/userPets', function (req, res) {
+    console.log('made it to userPets get!')
     if (req.isAuthenticated()) {
         // send back user object from database
         console.log('logged in', req.user);
-        console.log('/pets get hit');
+        console.log('/pets/userPets get hit');
         var userInfo = {
             username: req.user.username,
             id: req.user.id
@@ -20,7 +21,43 @@ router.get('/', function (req, res) {
                 res.sendStatus(500);
             } else {
                 // when connecting to database worked!
-                client.query('SELECT * FROM petlist WHERE user_id=$1', [userInfo.id], function (errorMakingQuery, result) {
+                client.query('SELECT * FROM petlist WHERE user_id = $1;', [req.user.id], function (errorMakingQuery, result) {
+                    done();
+                    if (errorMakingQuery) {
+                        console.log('Error making database query', errorMakingQuery);
+                        res.sendStatus(500);
+                    } else {
+                        console.log(result.rows);
+                        res.send(result.rows);
+                    }
+                });
+            }
+        });
+    } else {
+        console.log('not logged in');
+        res.sendStatus(403)
+    }
+});
+
+router.get('/', function (req, res) {
+    console.log('made it to userPets get!')
+    if (req.isAuthenticated()) {
+        // send back user object from database
+        console.log('logged in', req.user);
+        console.log('/pets/userPets get hit');
+        var userInfo = {
+            username: req.user.username,
+            id: req.user.id
+        };
+        console.log('id is', userInfo.id)
+        pool.connect(function (errorConnectingToDatabase, client, done) {
+            if (errorConnectingToDatabase) {
+                // when connecting to database failed
+                console.log('Error connecting to database', errorConnectingToDatabase);
+                res.sendStatus(500);
+            } else {
+                // when connecting to database worked!
+                client.query('SELECT * FROM petlist WHERE user_id = $1 && love = true;', [req.user.id], function (errorMakingQuery, result) {
                     done();
                     if (errorMakingQuery) {
                         console.log('Error making database query', errorMakingQuery);
